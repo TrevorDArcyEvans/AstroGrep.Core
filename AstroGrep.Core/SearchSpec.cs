0cba218d384a04e6e3c76a17263ea5ae24de74ee
Trevor D'Arcy-Evans <tdarcyevans@hotmail.com>
2023-02-16 11:25:40 +0000

    - ported to .NET Core 6 - removed all Windows specific code - removed all encoding detection

--- AstroGrep.Core/SearchSpec.cs
+++ AstroGrep.Core/SearchSpec.cs
@@ -1,65 +1,45 @@
-﻿using AstroGrep.Core.EncodingDetection;
-using System;
+﻿namespace AstroGrep.Core;
+
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace AstroGrep.Core
+/// <summary>
+/// Implement ISearchSpec interface.
+/// </summary>
+/// <history>
+/// [Curtis_Beard]		12/01/2014	Moved from frmMain.cs
+/// [Curtis_Beard]      02/09/2015	CHG: 92, support for specific file encodings
+/// [Curtis_Beard]      04/07/2015	CHG: remove line numbers
+/// [Curtis_Beard]	   05/26/2015	FIX: 69, add performance setting for file detection
+/// </history>
+public class SearchSpec : ISearchSpec
 {
-    /// <summary>
-    /// Implement ISearchSpec interface.
-    /// </summary>
-    /// <history>
-    /// [Curtis_Beard]		12/01/2014	Moved from frmMain.cs
-    /// [Curtis_Beard]      02/09/2015	CHG: 92, support for specific file encodings
-    /// [Curtis_Beard]      04/07/2015	CHG: remove line numbers
-    /// [Curtis_Beard]	   05/26/2015	FIX: 69, add performance setting for file detection
-    /// </history>
-    public class SearchSpec : ISearchSpec
-    {
-        /// <summary>starting directories</summary>
-        public List<string> StartDirectories { get; set; }
+  /// <summary>starting directories</summary>
+  public List<string> StartDirectories { get; set; } = new();
 
-        /// <summary>starting full file paths which if defined will ignore StartDirectories</summary>
-        public List<string> StartFilePaths { get; set; }
+  /// <summary>starting full file paths which if defined will ignore StartDirectories</summary>
+  public List<string> StartFilePaths { get; set; } = new();
 
-        /// <summary>search in sub folders</summary>
-        public bool SearchInSubfolders { get; set; }
+  /// <summary>search in sub folders</summary>
+  public bool SearchInSubfolders { get; set; }
 
-        /// <summary>search text will be used as a regular expression</summary>
-        public bool UseRegularExpressions { get; set; }
+  /// <summary>search text will be used as a regular expression</summary>
+  public bool UseRegularExpressions { get; set; }
 
-        /// <summary>enable case sensitive searching</summary>
-        public bool UseCaseSensitivity { get; set; }
+  /// <summary>enable case sensitive searching</summary>
+  public bool UseCaseSensitivity { get; set; }
 
-        /// <summary>enable whole word matching</summary>
-        public bool UseWholeWordMatching { get; set; }
+  /// <summary>enable whole word matching</summary>
+  public bool UseWholeWordMatching { get; set; }
 
-        /// <summary>enable detecting files that don't match the search text</summary>
-        public bool UseNegation { get; set; }
+  /// <summary>enable detecting files that don't match the search text</summary>
+  public bool UseNegation { get; set; }
 
-        /// <summary>number of context lines (0 is default)</summary>
-        public int ContextLines { get; set; }
+  /// <summary>number of context lines (0 is default)</summary>
+  public int ContextLines { get; set; }
 
-        /// <summary>the text to find</summary>
-        public string SearchText { get; set; }
+  /// <summary>the text to find</summary>
+  public string SearchText { get; set; }
 
-        /// <summary>enable only processing the file up until one match is found</summary>
-        public bool ReturnOnlyFileNames { get; set; }
-
-        /// <summary>list of FileEncodings objects to force encoding of certain files selected by user</summary>
-        public List<FileEncoding> FileEncodings { get; set; }
-
-        /// <summary>Current encoding options set by user</summary>
-        public EncodingOptions EncodingDetectionOptions { get; set; }
-
-        public SearchSpec()
-        {
-            StartDirectories = new List<string>();
-            StartFilePaths = new List<string>();
-            FileEncodings = new List<FileEncoding>();
-            EncodingDetectionOptions = new EncodingDetection.EncodingOptions();
-        }
-    }
+  /// <summary>enable only processing the file up until one match is found</summary>
+  public bool ReturnOnlyFileNames { get; set; }
 }
--- README.md
+++ README.md
@@ -1,10 +1,24 @@
 # AstroGrep.Core
 
-This is a fork of the original [AstroGrep repo](https://github.com/joshball/astrogrep). 
+This is a fork of [AstroGrep.Core](https://github.com/asherber/AstroGrep.Core), which, in turn, is a fork of
+the original [AstroGrep repo](https://github.com/joshball/astrogrep). 
 
-AstroGrep is a nice Windows grep utility, good speed and customizability. The app includes one assembly that is responsible for the actual searching. I wanted to be able to use just that functionality, so I pulled out that code and refactored a bit to remove references to other parts of the solution. I also renamed the assembly from `libAstroGrep.dll` to `AstroGrep.Core.dll`.
+```text
+AstroGrep is a nice Windows grep utility, good speed and customizability.
+The app includes one assembly that is responsible for the actual searching.
+I wanted to be able to use just that functionality, so I pulled out that code
+and refactored a bit to remove references to other parts of the solution.
+I also renamed the assembly from `libAstroGrep.dll` to `AstroGrep.Core.dll`.
+```
 
-Here's a sample usage:
+## Additional work
+* port to .NET Core 6
+* remove all Windows specific code
+  * removed all encoding detection 
+* ported to Linux
+
+## Sample usage:
+
 ```csharp
 var searchSpec = new SearchSpec()
 {
@@ -22,3 +36,6 @@ grep.Execute();
 var matchResults = grep.MatchResults;
 ```
 
+## Further work
+* Linux desktop UI
+* reinstate cross platform encoding detection
